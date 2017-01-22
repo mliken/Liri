29e9a42075a93d1c3c26115deabd4111d4bae6c7
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-01-22 14:42:26 -0600

    new keys

---
+++ keysExample.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+
+	consumer_key: 'Your key here',
+	consumer_secret: 'Your consumer secret here',
+	access_token_key: 'Your access token key here',
+	access_token_secret: 'Your access token secret here'
+}
